cfeec992f693a318211ed73f3af6df2b12469ea1
Matthias Kesenheimer <m.kesenheimer@gmx.net>
2023-03-26 16:25:16 +0200

    Small correction

--- ekscraper.py
+++ ekscraper.py
@@ -70,10 +70,13 @@ def collect(cfg):
 
             check = True
             for exclude in exclude_lst:
-                check = check and (exclude not in title.upper()) and (exclude not in descr.upper())
+                print(exclude)
+                check = check and (exclude.upper() not in title.upper()) and (exclude.upper() not in descr.upper())
+                print(check)
             
             if ("SUCHE" not in title.upper()) and ("SUCHE" not in descr.upper()) and check:
                 item_lst.append([title, descr, position, date, url,  price])
+                print(title)
         except:
             status = Status.FAIL
             log("Warning: parsing failed.")
