ca6557b1ca12bdb8d5bf704b99cf2ad0767d50cf
Matthias Kesenheimer <m.kesenheimer@gmx.net>
2023-06-03 22:55:53 +0200

    Added new run

--- run13.sh
+++ run13.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ./ekscraper.py -s "Tears of the kingdom" -l 40 -u 80 -e "Pins" "Poster" "Collector" "Koffer" "Metall" "Controller" "Amibo" "Amiibo" "Ambio" "Artbook" "Steelbook" "Sammel" "Coin" --intervall 3600
+python ./ekscraper.py -s "Tears of the kingdom" -l 35 -u 70 -e "Pins" "Poster" "Collector" "Koffer" "Metall" "Controller" "Amibo" "Amiibo" "Ambio" "Artbook" "Steelbook" "Sammel" "Coin" --intervall 3600
